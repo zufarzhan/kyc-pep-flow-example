be682e7c91de0472c1526db2ef10c53b7cd7826d
Zufar Ismanov <zufar.ismanov@gmail.com>
2023-04-02 14:35:33 +0200

    init

---
+++ components/ProfileMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-menu offset-y>
+    <template #activator="{ on }">
+      <button
+        type="button"
+        class="d-flex justify-center align-center"
+        v-on="on"
+      >
+        <v-avatar>
+          <img :src="userAvatar" alt="User Avatar" />
+        </v-avatar>
+        <span class="ml-3 d-none d-sm-flex">{{ userName }}</span>
+      </button>
+    </template>
+    <v-list>
+      <v-list-item v-for="(item, i) in menuItems" :key="i">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ item.name }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'ProfileMenu',
+  data() {
+    return {
+      userName: 'John Doe',
+      userAvatar: 'https://api.dicebear.com/6.x/bottts/svg?seed=Scooter',
+    }
+  },
+  computed: {
+    menuItems() {
+      const items = [
+        {
+          icon: 'mdi-account',
+          name: this.userName,
+        },
+        {
+          icon: 'mdi-cog',
+          name: 'Settings',
+        },
+        {
+          icon: 'mdi-face-agent',
+          name: 'Support',
+        },
+        {
+          icon: 'mdi-logout',
+          name: 'Logout',
+        },
+      ]
+      return items
+    },
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in drawerItems"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar clipped-left fixed app class="pr-sm-4">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <profile-menu />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      drawer: false,
+      drawerItems: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/dashboard',
+        },
+        {
+          icon: 'mdi-account-group',
+          title: 'Users',
+          to: '/users',
+        },
+        {
+          icon: 'mdi-credit-card-outline',
+          title: 'Payment',
+          to: '/',
+        },
+      ],
+      title: 'Company Y',
+    }
+  },
+}
+</script>
