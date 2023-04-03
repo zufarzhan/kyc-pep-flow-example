c11686ce6d847b7aa472b77efb63b6e370fe2e07
Zufar Ismanov <zufar.ismanov@gmail.com>
2023-04-03 18:56:23 +0200

    refactor(api): removed unused express middleware

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@ const router = require('./router');
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
 app.use('/api', router)
 
 app.use((err, req, res, next) => {
